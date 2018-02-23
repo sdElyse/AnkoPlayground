66de67be801c343434c6f67649157ec7f0b3d28c
Elyse Swoverland <elyseswoverland@gmail.com>
2018-02-23 12:03:13 -0500

    Trying out anko intents

--- app/src/main/java/com/elyseswoverland/ankoplayground/MainActivity.kt
+++ app/src/main/java/com/elyseswoverland/ankoplayground/MainActivity.kt
@@ -2,11 +2,10 @@ package com.elyseswoverland.ankoplayground
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.alert
+import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.Appcompat
-import org.jetbrains.anko.setContentView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
     lateinit var ui: MainActivityUi
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +13,35 @@ class MainActivity : AppCompatActivity() {
         ui = MainActivityUi()
         ui.setContentView(this)
 
+        info("MainActivity shown")
+
         ui.button.setOnClickListener {
-            alert(Appcompat, "Hello, ${ui.name.text}!", "Important Message") {
-                positiveButton("Well hi!") {}
-            }.show()
+            showAlert(ui.name.text.toString())
         }
     }
+
+    private fun showAlert(name: String) {
+
+        val dialog = alert(Appcompat, "I heard you like TeabagActivities", "Hello, $name!") {
+            positiveButton("Yes, he does") {}
+        }.show()
+//        if (dialog is AlertDialog) {
+////            dialog.window.setDimAmount(0.5f)
+//        }
+    }
+
+    private fun sendEmail(emailAddress: String) {
+        email(emailAddress, "Test email", "Test message")
+    }
+
+    private fun sendText(phoneNumber: String) {
+        sendSMS(phoneNumber, "They love me. Especially now that I can text you from this amazing app")
+    }
+
+    private fun shareText(text: String) {
+        share(text)
+    }
+
+    override val loggerTag: String
+        get() = "TAG"
 }
